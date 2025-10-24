2bc51b7c574c9e43b097e27a1275e396fcba6be2
Wenyu Zhu <wenyuzhu02@gmail.com>
2025-10-24 14:58:36 -0500

    Fix to main

--- spectre.c
+++ spectre.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include "lib/nospec.h"
+#include "./lib/nospec.h"
 #include <string.h>
-// #include "../inline_asm.h"
-// #include "../color.h"
-// #include <assert.h>
-// #include <ctype.h>
-// #include <inttypes.h>
-// #include <stdbool.h>
-// #include <sys/mman.h>
-// #include <x86intrin.h>
+#include <stdbool.h>
+#include "./lib/inline_asm.h"
+#include <assert.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <sys/mman.h>
+#include <x86intrin.h>
+#include <errno.h>
 
-uint8_t* array_idx;
+uint8_t* array;
+uint8_t* secret;
 volatile uint32_t arr_size = 8;
 
-uint32_t victim_function(uint32_t *arr, uint32_t index, uint32_t stride);
-void attacker_function(void);
-void init(void);
-
 void init(){
-    array_idx = (uint8_t *)malloc(arr_size);
+    array = (uint8_t *)malloc(arr_size);
+    secret = (uint8_t *)malloc(1);
+    *secret = "!";
     for(int i = 0; i < arr_size; i++){
-        array_idx[i] = i;
+        array[i] = i;
     }
 }
 
 int main (void){
-    //victim_function(index);
-    //
     init();
-    attacker_function();
-}
-void attacker_function(){
-	for(int i = 0; i < 1000; i++){
-        
-    }
+    attacker_function(array, secret);
 }
 
 uint32_t victim_function(uint32_t * arr, uint32_t index, uint32_t stride){
@@ -46,16 +38,16 @@ uint32_t victim_function(uint32_t * arr, uint32_t index, uint32_t stride){
         This leaves this code vulnerable to a Spectre attack that utilizes a side channel attack to extract the private index of the data array
     */
 
-    if(index > arr_size){
-        array_idx = realloc(array_idx, arr_size*2);
-        arr_size*=2;
-    }    
+    // if(index > arr_size){
+    //     array_idx = realloc(array_idx, arr_size*2);
+    //     arr_size*=2;
+    // }    
     
-    uint32_t new_idx = array_idx_nospec(index, arr_size);
-    uint32_t secret_idx = array_idx[new_idx];
+    // uint32_t new_idx = array_idx_nospec(index, arr_size);
+    // uint32_t secret_idx = array_idx[new_idx];
 
-//     return arr[secret_idx * stride];
-// }
+    // return arr[secret_idx * stride];
+}
 
 
 #define REP 100 // Number of repetitions to de-noise the channel
